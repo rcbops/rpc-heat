e45cdd8f424526fd5d7d23fb613baaae765d595c
Matt Thompson <mattt@defunct.ca>
2014-11-27 15:44:21 +0000

    Add ability to pass in Rackspace Cloud details, used for swift glance backend and MaaS

--- config_controller_primary.sh
+++ config_controller_primary.sh
@@ -137,6 +137,9 @@ EOF
 
 ifup -a
 
+rpc_user_config="/etc/rpc_deploy/rpc_user_config.yml"
+user_variables="/etc/rpc_deploy/user_variables.yml"
+
 echo "$PRIVATE_KEY" > .ssh/id_rsa
 chmod 600 .ssh/*
 
@@ -145,11 +148,15 @@ git clone -b %%RPC_VERSION%% %%GIT_REPO%%
 cd ansible-lxc-rpc
 pip install -r requirements.txt
 cp -a etc/rpc_deploy /etc/
-scripts/pw-token-gen.py --file /etc/rpc_deploy/user_variables.yml
-echo "nova_virt_type: qemu" >> /etc/rpc_deploy/user_variables.yml
 
-rpc_user_config="/etc/rpc_deploy/rpc_user_config.yml"
-user_variables="/etc/rpc_deploy/user_variables.yml"
+scripts/pw-token-gen.py --file $user_variables
+echo "nova_virt_type: qemu" >> $user_variables
+sed -i "s#\(rackspace_cloud_auth_url\): .*#\1: %%RACKSPACE_CLOUD_AUTH_URL%%#g" $user_variables
+sed -i "s#\(rackspace_cloud_tenant_id\): .*#\1: %%RACKSPACE_CLOUD_TENANT_ID%%#g" $user_variables
+sed -i "s#\(rackspace_cloud_username\): .*#\1: %%RACKSPACE_CLOUD_USERNAME%%#g" $user_variables
+sed -i "s#\(rackspace_cloud_password\): .*#\1: %%RACKSPACE_CLOUD_PASSWORD%%#g" $user_variables
+sed -i "s#\(rackspace_cloud_api_key\): .*#\1: %%RACKSPACE_CLOUD_API_KEY%%#g" $user_variables
+
 environment_version=$(md5sum /etc/rpc_deploy/rpc_environment.yml | awk '{print $1}')
 
 curl -o $rpc_user_config https://raw.githubusercontent.com/mattt416/rpc_heat/master/rpc_user_config.yml

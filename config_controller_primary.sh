08f08011a3ddd4b314b081b44ad81bf39fc70b9d
Matt Thompson <mattt@defunct.ca>
2014-12-18 16:42:52 +0000

    Primary controller script changes
    Running this heat stack against stable/icehouse fails as
    keystone-all.yml does not exist in stable/icehouse.  This change
    basically filters out the logging-related playbooks from
    openstack-setup.yml so we do not have to manually maintain the list of
    playbooks to run.

--- config_controller_primary.sh
+++ config_controller_primary.sh
@@ -177,6 +177,11 @@ cp -a etc/rpc_deploy /etc/
 
 scripts/pw-token-gen.py --file $user_variables
 echo "nova_virt_type: qemu" >> $user_variables
+cat >> $user_variables << "EOF"
+apt_common_repos:
+  - { repo: "deb http://lon1.mirrors.digitalocean.com/mariadb/repo/5.5/ubuntu {{ ansible_distribution_release }} main", state: "present" }
+EOF
+
 sed -i "s#\(rackspace_cloud_auth_url\): .*#\1: %%RACKSPACE_CLOUD_AUTH_URL%%#g" $user_variables
 sed -i "s/\(rackspace_cloud_tenant_id\): .*/\1: %%RACKSPACE_CLOUD_TENANT_ID%%/g" $user_variables
 sed -i "s/\(rackspace_cloud_username\): .*/\1: %%RACKSPACE_CLOUD_USERNAME%%/g" $user_variables
@@ -199,15 +204,10 @@ if [ "$ANSIBLE_PLAYBOOKS" = "all" ]; then
   retry 3 ansible-playbook -e @${user_variables} playbooks/infrastructure/infrastructure-setup.yml \
                                                  playbooks/openstack/openstack-setup.yml
 else
+  egrep -v 'rpc-support-all.yml|rsyslog-config.yml' playbooks/openstack/openstack-setup.yml > \
+                                                    playbooks/openstack/openstack-setup-no-logging.yml
   retry 3 ansible-playbook -e @${user_variables} playbooks/infrastructure/memcached-install.yml \
                                                  playbooks/infrastructure/galera-install.yml \
                                                  playbooks/infrastructure/rabbit-install.yml
-  retry 3 ansible-playbook -e @${user_variables} playbooks/openstack/keystone-all.yml \
-                                                 playbooks/openstack/glance-all.yml \
-                                                 playbooks/openstack/heat-all.yml \
-                                                 playbooks/openstack/nova-all.yml \
-                                                 playbooks/openstack/neutron-all.yml \
-                                                 playbooks/openstack/cinder-all.yml \
-                                                 playbooks/openstack/horizon-all.yml \
-                                                 playbooks/openstack/utility-all.yml
+  retry 3 ansible-playbook -e @${user_variables} playbooks/openstack/openstack-setup-no-logging.yml
 fi

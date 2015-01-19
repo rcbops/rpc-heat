6796237baa015f4de158a3fb0e78f5a93df224cb
Matt Thompson <mattt@defunct.ca>
2015-01-19 09:58:29 +0000

    Add temporary commands to test new neutron library

--- jenkins/stack-deploy.sh
+++ jenkins/stack-deploy.sh
@@ -5,6 +5,11 @@ set -e
 source ~/.openrc
 
 ip=$(heat output-show rpc-jenkins-${BUILD_NUMBER} controller1_ip | sed -e 's/"//g')
-cmd="cd /root/ansible-lxc-rpc/rpc_deployment && bash run_ansible.sh"
+checkout="/root/ansible-lxc-rpc/rpc_deployment"
+ssh_key=~/.ssh/jenkins
 
-ssh -l root -i ~/.ssh/jenkins -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $ip $cmd
+# Temporary commands to test new neutron library
+ssh -l root -i $ssh_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $ip "cd $checkout && curl -o library/neutron https://gist.githubusercontent.com/mattt416/de164b5737c475b2b9ac/raw/efc181f59c6173cdbad8ef68fee2f6efa9f13173/gistfile1.txt"
+scp -i $ssh_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null jenkins/tempest_resources.yml root@${ip}:${checkout}/roles/tempest_resources/tasks/main.yml
+
+ssh -l root -i $ssh_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $ip "cd $checkout && bash run_ansible.sh"

d7c95217f1b8ef20900a16867f6476a19712e6e2
Matt Thompson <mattt@defunct.ca>
2015-01-20 10:42:34 +0000

    More temporary changes to test tempest

--- jenkins/stack-deploy.sh
+++ jenkins/stack-deploy.sh
@@ -10,8 +10,9 @@ ssh_key=~/.ssh/jenkins
 ssh_options="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
 # Temporary commands to test new neutron library
-ssh -l root -i $ssh_key $ssh_options $ip "cd $checkout && curl -o library/neutron https://gist.githubusercontent.com/mattt416/de164b5737c475b2b9ac/raw/efc181f59c6173cdbad8ef68fee2f6efa9f13173/gistfile1.txt"
+ssh -l root -i $ssh_key $ssh_options $ip "cd $checkout && curl -o library/neutron https://raw.githubusercontent.com/mattt416/ansible-lxc-rpc/0cfc78d5749f7178995242272e5e90f1202e564d/rpc_deployment/library/neutron"
 scp -i $ssh_key $ssh_options jenkins/tempest_resources.yml root@${ip}:${checkout}/roles/tempest_resources/tasks/main.yml
+scp -i $ssh_key $ssh_options jenkins/tempest.conf.j2 root@${ip}:${checkout}/roles/tempest/templates/tempest.conf.j2
 
 ssh -l root -i $ssh_key $ssh_options $ip "cd $checkout && bash run_ansible.sh"
 

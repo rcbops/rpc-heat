176698c053c64bdc20b40794800fa678c3476935
Darren Birkett <darren.birkett@gmail.com>
2015-01-22 13:18:54 +0000

    wrap git clone in a conditional
    - allows manual running of script - can be handy sometimes if the run
      failed initially
    - also s/ansible-lxc-rpc/os-ansible-deployment/

--- jenkins/stack-deploy.sh
+++ jenkins/stack-deploy.sh
@@ -5,19 +5,19 @@ set -e
 source ~/.openrc
 
 ip=$(heat output-show rpc-jenkins-${BUILD_NUMBER} controller1_ip | sed -e 's/"//g')
-checkout="/root/ansible-lxc-rpc/rpc_deployment"
+checkout="/root/os-ansible-deployment/rpc_deployment"
 ssh_key=~/.ssh/jenkins
 ssh_options="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
 # Temporary commands to test new neutron library
-ssh -l root -i $ssh_key $ssh_options $ip "cd $checkout && curl -o library/neutron https://raw.githubusercontent.com/mattt416/ansible-lxc-rpc/3b425cdd1f7ef8ca8819c503bcda7e0b0dfa1351/rpc_deployment/library/neutron"
+ssh -l root -i $ssh_key $ssh_options $ip "cd $checkout && curl -o library/neutron https://raw.githubusercontent.com/mattt416/os-ansible-deployment/3b425cdd1f7ef8ca8819c503bcda7e0b0dfa1351/rpc_deployment/library/neutron"
 scp -i $ssh_key $ssh_options jenkins/tempest_resources.yml root@${ip}:${checkout}/roles/tempest_resources/tasks/main.yml
 scp -i $ssh_key $ssh_options jenkins/tempest.conf.j2 root@${ip}:${checkout}/roles/tempest/templates/tempest.conf.j2
 
 # Temporary work-around otherwise we hit https://bugs.launchpad.net/neutron/+bug/1382064
 # which results in tempest tests failing
-ssh -l root -i $ssh_key $ssh_options $ip "sed -i 's/api_workers = 10/api_workers = 0/' /root/ansible-lxc-rpc/rpc_deployment/roles/neutron_common/templates/neutron.conf"
-ssh -l root -i $ssh_key $ssh_options $ip "sed -i 's/rpc_workers = 5/rpc_workers = 0/' /root/ansible-lxc-rpc/rpc_deployment/roles/neutron_common/templates/neutron.conf"
+ssh -l root -i $ssh_key $ssh_options $ip "sed -i 's/api_workers = 10/api_workers = 0/' /root/os-ansible-deployment/rpc_deployment/roles/neutron_common/templates/neutron.conf"
+ssh -l root -i $ssh_key $ssh_options $ip "sed -i 's/rpc_workers = 5/rpc_workers = 0/' /root/os-ansible-deployment/rpc_deployment/roles/neutron_common/templates/neutron.conf"
 
 ssh -l root -i $ssh_key $ssh_options $ip "cd $checkout && bash run_ansible.sh"
 

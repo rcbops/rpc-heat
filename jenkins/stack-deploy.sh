9bd980ba0fc37ce479e985f07c24cb14ff6a65c9
Matt Thompson <mattt@defunct.ca>
2015-01-27 10:48:34 +0000

    Only run tempest if tempest in ANSIBLE_PLAYBOOKS and tee output to file

--- jenkins/stack-deploy.sh
+++ jenkins/stack-deploy.sh
@@ -14,8 +14,10 @@ ssh_options="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 ssh -l root -i $ssh_key $ssh_options $ip "sed -i 's/api_workers = 10/api_workers = 0/' /root/os-ansible-deployment/rpc_deployment/roles/neutron_common/templates/neutron.conf"
 ssh -l root -i $ssh_key $ssh_options $ip "sed -i 's/rpc_workers = 5/rpc_workers = 0/' /root/os-ansible-deployment/rpc_deployment/roles/neutron_common/templates/neutron.conf"
 
-ssh -l root -i $ssh_key $ssh_options $ip "cd $checkout && bash run_ansible.sh"
+ssh -l root -i $ssh_key $ssh_options $ip "cd $checkout && bash run_ansible.sh 2>&1 | tee -a run_ansible.log"
 
 ssh -l root -i $ssh_key $ssh_options $ip "ifconfig br-vlan 10.1.13.1 netmask 255.255.255.0"
 
-ssh -l root -i $ssh_key $ssh_options $ip "lxc-attach -n \$(lxc-ls | grep utility) -- sh -c 'cd /opt/tempest_*/ && ./run_tempest.sh --smoke -N || true'"
+if echo "$ANSIBLE_PLAYBOOKS" | grep "tempest"; then
+  ssh -l root -i $ssh_key $ssh_options $ip "lxc-attach -n \$(lxc-ls | grep utility) -- sh -c 'cd /opt/tempest_*/ && ./run_tempest.sh --smoke -N || true'"
+fi

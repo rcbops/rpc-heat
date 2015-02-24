c09735391fc0b31563c0e0e004b11bf668981e4e
Matt Thompson <mattt@defunct.ca>
2015-02-24 14:44:16 +0000

    Update old reference to True
    DEPLOY_SWIFT should now be "yes" or "no", not "True" or "False".

--- config_controller_primary.sh
+++ config_controller_primary.sh
@@ -202,7 +202,7 @@ sed -i "s/\(rackspace_cloud_api_key\): .*/\1: %%RACKSPACE_CLOUD_API_KEY%%/g" $us
 sed -i "s/\(glance_default_store\): .*/\1: %%GLANCE_DEFAULT_STORE%%/g" $user_variables
 sed -i "s/\(maas_notification_plan\): .*/\1: npTechnicalContactsEmail/g" $user_variables
 
-if [ "%%DEPLOY_SWIFT%%" = "True" ]; then
+if [ "%%DEPLOY_SWIFT%%" = "yes" ]; then
   sed -i "s/\(glance_swift_store_auth_address\): .*/\1: '{{ auth_identity_uri }}'/" $user_variables
   sed -i "s/\(glance_swift_store_key\): .*/\1: '{{ glance_service_password }}'/" $user_variables
   sed -i "s/\(glance_swift_store_region\): .*/\1: RegionOne/" $user_variables
